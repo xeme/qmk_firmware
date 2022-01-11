d1d6bb0b745bed1417d1f0f6f5b07f73c4f5345d
Filip <filip@air.lan>
2022-01-11 01:21:09 +0100

    Make shift/space mod-tap key behave more as expected.

---
+++ keyboards/splitkb/kyria/keymaps/xeme/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Smarter dual keys, especially useful for space/shift.
+#define HOLD_ON_OTHER_KEY_PRESS
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM 500
+#define TAPPING_FORCE_HOLD_PER_KEY
+
