d56167ed322432cd207050151780a0e5fe9eac12
Filip <filip@air.lan>
2022-01-15 01:01:07 +0100

    - Add layer toggle on hold - Add RGB for layers - Add override over numbers to make cmd work with base layer - Make cmd arrows be home and end

--- keyboards/splitkb/kyria/keymaps/xeme/config.h
+++ keyboards/splitkb/kyria/keymaps/xeme/config.h
@@ -5,3 +5,8 @@
 #define PERMISSIVE_HOLD
 #define TAPPING_TERM 200
 #define TAPPING_FORCE_HOLD_PER_KEY
+
+// Used by TT to toggle layers on signle tap.
+#define TAPPING_TOGGLE 1
+
+#define RGBLIGHT_LAYERS
--- keyboards/splitkb/kyria/keymaps/xeme/keymap.c
+++ keyboards/splitkb/kyria/keymaps/xeme/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO, KC_A, KC_S, KC_D, KC_F, KC_G,                              KC_H, KC_J, KC_K,    KC_L,   KC_QUOT, KC_NO,
     KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO, KC_NO,  KC_NO, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_NO,
 
-    LCTL_T(KC_TAB), LGUI_T(KC_ESC), TG(1), KC_NO, KC_NO, LALT_T(KC_ENT), KC_SPC, LSFT_T(KC_BSPC), KC_NO),
+    LCTL_T(KC_TAB), LGUI_T(KC_ESC), TT(1), KC_NO, KC_NO, LALT_T(KC_ENT), KC_SPC, LSFT_T(KC_BSPC), KC_NO),
 
 	[1] = LAYOUT(
     KC_NO, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,                                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
@@ -53,6 +53,36 @@ const key_override_t word_backspace = ko_make_basic(
   LSFT_T(KC_BSPC),
   LALT(KC_BSPC));
 
+const key_override_t cmd_q_on_second_layer = ko_make_basic(
+  MOD_MASK_GUI,
+  KC_1,
+  LGUI(KC_Q));
+
+const key_override_t cmd_w_on_second_layer = ko_make_basic(
+  MOD_MASK_GUI,
+  KC_2,
+  LGUI(KC_W));
+
+const key_override_t cmd_e_on_second_layer = ko_make_basic(
+  MOD_MASK_GUI,
+  KC_3,
+  LGUI(KC_E));
+
+const key_override_t cmd_r_on_second_layer = ko_make_basic(
+  MOD_MASK_GUI,
+  KC_4,
+  LGUI(KC_R));
+
+const key_override_t home_override = ko_make_basic(
+  MOD_MASK_GUI,
+  KC_LEFT,
+  KC_HOME);
+
+const key_override_t end_override = ko_make_basic(
+  MOD_MASK_GUI,
+  KC_RIGHT,
+  KC_END);
+
 /*
 const key_override_t next_app_override = ko_make_basic(
   MOD_MASK_CTRL,
@@ -80,6 +110,12 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     &page_up_second_layer,
     &page_down_second_layer,
     &word_backspace,
+    &cmd_q_on_second_layer,
+    &cmd_w_on_second_layer,
+    &cmd_e_on_second_layer,
+    &cmd_r_on_second_layer,
+    &home_override,
+    &end_override,
     // &next_app_override,
     // &previous_app_override,
     // These won't work for now because they have conflicting modifiers. This
@@ -90,3 +126,21 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 //    &previous_window_override,
     NULL // Null terminate the array of overrides!
 };
+
+void default_led(void) {
+  // Default hue saturation and value.
+  rgblight_sethsv(80, 255, 100);
+}
+
+void keyboard_post_init_user(void) {
+   default_led();
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    if (layer_state_cmp(state, 1)) {
+      rgblight_sethsv(80, 255, 255);
+    } else {
+      default_led();
+    }
+    return state;
+}
